9821a2b87fa6113e939b9f1164caee07feb61bef
KamilaAkhmetova <156825763+KamilaAkhmetova@users.noreply.github.com>
2026-04-14 21:53:29 +0500

    minor changes with menu

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected isLoggedIn = signal(false);
+  protected isMenuOpen = signal(false);  
+
+  constructor(private router: Router) {
+    const token = localStorage.getItem('token');
+    this.isLoggedIn.set(!!token);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.isLoggedIn.set(false);
+    this.router.navigate(['/login']);
+  }
 }
