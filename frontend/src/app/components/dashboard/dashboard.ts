50ada24e7953285ae00a8e795b280981bacabbb7
KamilaAkhmetova <156825763+KamilaAkhmetova@users.noreply.github.com>
2026-04-16 13:31:31 +0500

    changed

--- frontend/src/app/components/dashboard/dashboard.ts
+++ frontend/src/app/components/dashboard/dashboard.ts
@@ -1,27 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-<<<<<<< Updated upstream
 import { RouterModule } from '@angular/router';
-
-@Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, RouterModule],  
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
-})
-export class DashboardComponent {
-=======
-import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './dashboard.html',
     styleUrls: ['./dashboard.css']
 })
-export class Dashboard {
+export class DashboardComponent {
     constructor() {}
->>>>>>> Stashed changes
 }
