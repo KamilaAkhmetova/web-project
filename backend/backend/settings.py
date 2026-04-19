51d8fe9b57f25858e02543518c65464bf93bb7f6
KamilaAkhmetova <156825763+KamilaAkhmetova@users.noreply.github.com>
2026-04-20 02:46:08 +0500

    database connected

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "auth_app",
     'buffets',
     "dashboard",
+    "schedules",
 ]
 
 MIDDLEWARE = [
@@ -80,9 +81,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
--- backend/schedules/models.py
+++ backend/schedules/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings  
 
-# Create your models here.
+class Course(models.Model):
+    LESSON_TYPES = [
+        ('lecture', 'Lecture'),
+        ('practice', 'Practice'),
+        ('lab', 'Laboratory'),
+    ]
+    
+    name = models.CharField(max_length=200, verbose_name="Name")
+    code = models.CharField(max_length=20, verbose_name="Course code", unique=True)
+    credits = models.IntegerField(verbose_name="Credits")
+    lesson_type = models.CharField(max_length=20, choices=LESSON_TYPES, verbose_name="Lesson type")
+    lecturer = models.CharField(max_length=100, verbose_name="Lecturer")
+    room = models.CharField(max_length=50, verbose_name="Room")
+    description = models.TextField(blank=True, verbose_name="Description")
+    
+    def __str__(self):
+        return f"{self.code}: {self.name}"
+    
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+
+class Schedule(models.Model):
+    """Student schedule"""
+    DAYS_OF_WEEK = [
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),
+        (7, 'Sunday'),
+    ]
+    
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='schedule')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    day_of_week = models.IntegerField(choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    
+    class Meta:
+        unique_together = ['student', 'course', 'day_of_week', 'start_time']
+        ordering = ['day_of_week', 'start_time']
+    
+    def __str__(self):
+        return f"{self.student.username} - {self.course.name} ({self.get_day_of_week_display()})"
