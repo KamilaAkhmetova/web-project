ef548107216c2a9aca3c88b34914d45d54645b5f
KamilaAkhmetova <156825763+KamilaAkhmetova@users.noreply.github.com>
2026-04-21 18:30:49 +0500

    changed schedules

--- frontend/src/app/components/schedules/schedules.ts
+++ frontend/src/app/components/schedules/schedules.ts
@@ -29,7 +29,7 @@ export class Schedules implements OnInit {
   isLoading = true;
 
   // не уверена
-  currentWeek = '11th';
+  currentWeek = '14th';
   // timeSlots = [
   //   '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', 
   //   '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'
@@ -49,7 +49,7 @@ export class Schedules implements OnInit {
     generateTimeSlots() {
         for (let hour = 8; hour <= 20; hour++) {
             this.timeSlots.push(`${hour}:00`);
-            this.timeSlots.push(`${hour}:30`);
+            // this.timeSlots.push(`${hour}:30`);
         }
     }
     
@@ -60,11 +60,11 @@ export class Schedules implements OnInit {
         const diff = day === 0 ? 6 : day - 1;
         monday.setDate(today.getDate() - diff);
         
-        for (let i = 0; i < 5; i++) {
-            const date = new Date(monday);
-            date.setDate(monday.getDate() + i);
-            this.weekDays[i].date = `${date.getDate()}.${date.getMonth() + 1}.`;
-        }
+        for (let i = 0; i < this.weekDays.length; i++) {
+          const date = new Date(monday);
+          date.setDate(monday.getDate() + i);
+          this.weekDays[i].date = `${date.getDate()}.${date.getMonth() + 1}.`;
+      }
     }
     
     loadSchedule() {
@@ -98,35 +98,3 @@ export class Schedules implements OnInit {
 }
 
 
-//   ngOnInit() {
-//     this.loadSchedule();
-//   }
-  
-//   loadSchedule() {
-//     // TODO: загрузить расписание из API
-//     // Пока тестовые данные
-//     this.scheduleData = [
-//       { day: 1, time: '10:00', course: { name: 'Web Development', lesson_type: 'lecture', room: 'B/E105' } },
-//       { day: 1, time: '13:00', course: { name: 'Database Systems', lesson_type: 'practice', room: 'B/N203' } },
-//       { day: 2, time: '9:00', course: { name: 'Machine Learning', lesson_type: 'lecture', room: 'T12/S' } },
-//       { day: 3, time: '11:00', course: { name: 'Programming Languages', lesson_type: 'lecture', room: 'B/L314' } },
-//       { day: 4, time: '14:00', course: { name: 'Human Resources', lesson_type: 'seminar', room: 'Online' } },
-//       { day: 5, time: '8:00', course: { name: 'Software Engineering', lesson_type: 'lab', room: 'C228' } }
-//     ];
-//   }
-  
-//   // Получить занятие по дню и времени
-//   getLesson(dayIndex: number, timeSlot: string): any {
-//     return this.scheduleData.find(lesson => 
-//       lesson.day === dayIndex && lesson.time === timeSlot
-//     );
-//   }
-  
-//   // Открыть модальное окно с деталями занятия
-//   openLessonModal(lesson: any) {
-//     if (lesson) {
-//       console.log('Open lesson:', lesson);
-//       // TODO: открыть модальное окно
-//     }
-//   }
-// }
