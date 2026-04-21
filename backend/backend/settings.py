c043445560fc2a26b1f45d0d0dbfbc93829b3ada
KamilaAkhmetova <156825763+KamilaAkhmetova@users.noreply.github.com>
2026-04-21 23:24:36 +0500

    db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,13 +90,8 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-print("DB:", config('DB_NAME'))
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# print("DB:", config('DB_NAME'))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
